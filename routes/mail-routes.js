edd957bd0caf1a9cd492e9a7ab6a1f804c5b041e
OKali <o.kalinowski27@gmail.com>
2021-05-12 19:40:31 -0400

    nodemailer/ mail route - Test FAILED, include user and pass

--- routes/mail-routes.js
+++ routes/mail-routes.js
@@ -4,6 +4,32 @@ var nodemailer = require("nodemailer");
 
 module.exports = function(app) {
     app.post("/contact-form", (req, res) => {
-        //nodemailer, gmail, transporter go here
-    })
+       let transporter = nodemailer.createTransport({
+           service: "gmail",
+           auth: {
+               user: process.env.EMAIL,
+               pass: process.env.PASSWORD
+           },
+       });
+
+       let mailData = {
+           from: process.env.EMAIL,
+           to: process.env.EMAIL,
+           subject: `Contact Form from Porfolio`,
+           text: `Contact Form from Portfolio from
+           name: ${req.body.name},
+           email: ${req.body.email},
+           message: ${req.body.message}`
+       };
+
+       transporter.sendMail(mailData).then(function(response) {
+           console.log(`Email sent with promise`)
+       }).catch(function (error) {
+           console.log(`Error occured: ${error}`);
+       });
+
+        res.send(`Thank You for sending an email to Decyma Construction. 
+             We will get back to your as soon as possible.`);
+//res.sendFile(path.join(__dirname, "../public/contact-form-sent.html"));
+    });
 }
