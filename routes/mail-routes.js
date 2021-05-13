5bcb97969591c6c269b1f0eae3997defd2bb6481
OKali <o.kalinowski27@gmail.com>
2021-05-13 10:31:28 -0400

    Nodemailer Email Test -- SUCCESSFUL!

--- routes/mail-routes.js
+++ routes/mail-routes.js
@@ -2,34 +2,37 @@ var path = require("path");
 require("dotenv").config();
 var nodemailer = require("nodemailer");
 
-module.exports = function(app) {
-    app.post("/contact-form", (req, res) => {
-       let transporter = nodemailer.createTransport({
-           service: "gmail",
-           auth: {
-               user: process.env.EMAIL,
-               pass: process.env.PASSWORD
-           },
-       });
+module.exports = function (app) {
+  app.post("/contact-form-sent", (req, res) => {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
 
-       let mailData = {
-           from: process.env.EMAIL,
-           to: process.env.EMAIL,
-           subject: `Contact Form from Porfolio`,
-           text: `Contact Form from Portfolio from
+    let mailData = {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: `Contact Form from Porfolio`,
+      text: `Contact Form from Portfolio from
            name: ${req.body.name},
            email: ${req.body.email},
-           message: ${req.body.message}`
-       };
+           message: ${req.body.message}`,
+    };
 
-       transporter.sendMail(mailData).then(function(response) {
-           console.log(`Email sent with promise`)
-       }).catch(function (error) {
-           console.log(`Error occured: ${error}`);
-       });
+    transporter
+      .sendMail(mailData)
+      .then(function (response) {
+        console.log(`Email sent with promise`);
+      })
+      .catch(function (error) {
+        console.log(`Error occured: ${error}`);
+      });
 
-        res.send(`Thank You for sending an email to Decyma Construction. 
+    res.send(`Thank You for sending an email to Olivia Kalinowski Development. 
              We will get back to your as soon as possible.`);
-//res.sendFile(path.join(__dirname, "../public/contact-form-sent.html"));
-    });
-}
+    //res.sendFile(path.join(__dirname, "../public/contact-form-sent.html"));
+  });
+};
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require('./routes/html-routes')(app);
+require('./routes/mail-routes')(app);
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
